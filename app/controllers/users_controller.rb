cb7a68c88db722a0538b58e56521445ecaeae313
shyuhei-kaneko <shyhei0709@gmail.com>
2019-03-02 00:15:28 +0000

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  
+  def index
+    @users = User.all
+  end
+  
   def show
     @user = User.find(params[:id])
   end
@@ -21,11 +27,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
@@ -41,10 +45,20 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
     
+    # beforeアクション
+
+    # ログイン済みユーザーか確認
     def logged_in_user
       unless logged_in?
-        flash[:dangerous] = "ログインしてください。"
-      redirect_to login_url
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
       end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
