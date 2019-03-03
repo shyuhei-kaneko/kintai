10fca1ae148b69d1d089a3e115d755997df79734
shyuhei-kaneko <shyhei0709@gmail.com>
2019-03-03 02:16:54 +0000

    Finish user edit index destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@
 User.create!(name:  "管理者",
              email: "email@sample.com",
              password:              "password",
-             password_confirmation: "password")
+             password_confirmation: "password",
+             admin: true)
 
 59.times do |n|
   name  = Faker::Name.name
