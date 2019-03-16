1c937268c438b445c7ac35cf9b13b206a3ccace9
shyuhei-kaneko <shyhei0709@gmail.com>
2019-03-16 01:14:17 +0000

    Finish Attendances edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
         record.save
       end
     end
-    @dates = @user.attendances.where('worked_on >= ? and worked_on <= ?', @first_day, @last_day)
+    @dates = user_attendances_month_date
     @worked_sum = @dates.where.not(started_at: nil).count
   end
   
